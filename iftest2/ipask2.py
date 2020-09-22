cca01acbe6267338a9cec5a2c0ccfaa1989cef45
Chris Calso <christopher.calso@gmail.com>
2020-09-22 17:02:34 +0000

    testing if conditionals

--- iftest2/ipask2.py
+++ iftest2/ipask2.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+import ipaddress
 ipchk = input("Apply an IP address: ") # this line now prompts the user for input
 
 # if user set IP of gateway
-if ipchk == "192.168.70.1":
-   print("Looks like the IP address of the Gateway was set: " + ipchk + " This is not recommended.")
-elif ipchk: # if any data is provided, this will test true
+if ipchk  == "192.168.70.1":
+    ipaddress == ipaddress.ip_address(ipchk)
+    print("Looks like the IP address of the Gateway was set: " + ipaddress + " This is not recommended.")
+
+elif ipchk.ip_address(): # if any data is provided, this will test true
    print("Looks like the IP address was set: " + ipchk) # indented under if
 else: # if data is NOT provided
    print("You did not provide input.") # indented under else
